79c6e029013926af4c519ad8c46e2d8762fe8bb9
mdyoung3 <mdyoung3@stanford.edu>
2025-03-10 17:54:02 -0600

    Test config tweak

--- tests/codeception/acceptance/SystemCest.php
+++ tests/codeception/acceptance/SystemCest.php
@@ -16,7 +16,7 @@ class SystemCest {
     $I->amOnPage('/admin/reports/status');
     $I->canSee('10.4', '.system-status-general-info');
     if ($I->grabMultiple('.system-status-counter--error')) {
-      $I->canSee('1 Error', '.system-status-counter--error');
+      $I->canSee('2 Error', '.system-status-counter--error');
       $I->canSee('Access to update.php ', '.system-status-report__status-icon--error');
     }
 
